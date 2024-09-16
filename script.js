2d5f60e8d1b754b77a03ced5a69c76b1ff5c5843
luvkumarcs <luvkumarcs@gmail.com>
2024-09-16 17:31:35 +0530

    some Changes

--- script.js
+++ script.js
@@ -67,5 +67,117 @@ gsap.from(" section.home .first .sales", {
     stagger: 0.3
 })
 
+gsap.from("section.about .container #out",{
+    opacity: 0,
+    duration: 2,
+    x:-250,
+    scrollTrigger:"section.about .container #out",
+})
+
+gsap.from("section.about .container #in",{
+    opacity: 0,
+    duration: 2,
+    y:250,
+    scrollTrigger:"section.about .container #in",
+})
+
+gsap.from("section.about .container #intl",{
+    opacity: 0,
+    duration: 2,
+    y:-250,
+    scrollTrigger:"section.about .container #intl",
+})
+
+gsap.from("section.about .container #appt",{
+    opacity: 0,
+    duration: 2,
+    x:250,
+    scrollTrigger:"section.about .container #appt",
+})
+
+gsap.from("section.carrier #unique",{
+    opacity: 0,
+    duration: 2,
+    x:250,
+    scrollTrigger:"section.carrier #unique",
+})
+
+gsap.from("section.carrier #ambassadors",{
+    opacity: 0,
+    duration: 2,
+    x:-250,
+    scrollTrigger:"section.carrier #ambassadors",
+})
+
+gsap.from("section.carrier #fit",{
+    opacity: 0,
+    duration: 2,
+    x:250,
+    scrollTrigger:"section.carrier #fit",
+})
+
+gsap.from("section.contact .client #chris ",{
+    opacity: 0,
+    duration: 2,
+    x:-250,
+    scrollTrigger:"section.contact .client #chris ",  
+})
+
+gsap.from("section.contact .client #ken ",{
+    opacity: 0,
+    scale:0,
+    duration: 3,
+    scrollTrigger:"section.contact .client #ken ",  
+})
+
+gsap.from("section.contact .client #dave ",{
+    opacity: 0,
+    duration: 2,
+    x:250,
+    scrollTrigger:"section.contact .client #dave ",  
+})
+
+gsap.from("section.carrier .icon-container .icon",{
+    opacity: 0,
+    duration: 2,
+    x:-250,
+    stagger: 0.3,
+    scrollTrigger:"section.carrier .icon-container .icon",
+})
+
+gsap.from("section.carrier .icon-container .tick",{
+    opacity: 0,
+    duration: 2,
+    x:-250,
+    stagger: 0.3,
+    scrollTrigger:"section.carrier .icon-container .icon",
+})
+
+// ------------------------------for about section-------------------------
+
+gsap.from("section.about .team_member .ben", {
+    opacity: 0,
+    duration: 2,
+    x:-250,
+    // stagger: 0.3,
+    scrollTrigger:"section.about .team_member .ben",
+})
+
+gsap.from("section.about .team_member .cody", {
+    opacity: 0,
+    scale:0,
+    duration: 3,
+    // x:-250,
+    // stagger: 0.3,
+    scrollTrigger:"section.about .team_member .cody",
+})
+
+gsap.from("section.about .team_member .nadeem", {
+    opacity: 0,
+    duration: 2,
+    x:250,
+    // stagger: 0.3,
+    scrollTrigger:"section.about .team_member .nadeem",
+})
 
 
